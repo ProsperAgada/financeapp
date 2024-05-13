2cd36dba0ce37bcc3f57fcd9bdce30fe94a85b01
ProsperAgada <prosperagada@nvit.tech>
2024-05-13 05:53:05 -0700

    adding src

---
+++ app/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from .models import posts
+from django.views import generic
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html' )
+def about(request):
+    return render(request, 'about.html' )
+def contact(request):
+    return render(request, 'contact.html' )
+def service(request):
+    return render(request, 'service.html' )
+def testimonial(request):
+    return render(request, 'testimonial.html' )
+def team(request):
+    return render(request, 'team.html' )
+def error404(request):
+    return render(request, '404.html' )
+def home(request):
+    return render(request, 'home.html' )
+def careers(request):
+    return render(request, 'career.html' )
+
+
+# industires links
+def industries(request):
+    return render(request, 'industries.html' )
+def consumer_industrial_market(request):
+    return render(request, 'consumer-industrial-markets.html' )
+def financial_service_industry(request):
+    return render(request, 'finanacial-service-industry.html' )
+def energy_natural_resources(request):
+    return render(request, 'energy-natural-resources.html' )
+def public_social_sector(request):
+    return render(request, 'public-social-sector.html' )
+def real_estate_construction(request):
+    return render(request, 'real-estate-construction.html' )
+def technology_media_telecommunication(request):
+    return render(request, 'technology-media-telecommunication.html' )
+def transport_logistics(request):
+    return render(request, 'transport-logistics.html' )
+
+# services link
+def services(request):
+    return render(request, 'services.html' )
+def advisory(request):
+    return render(request, 'advisory.html' )
+def audit_assurance(request):
+    return render(request, 'audit-assurance.html' )
+def consulting(request):
+    return render(request, 'consulting.html' )
+def tax_regulatory_people_services(request):
+    return render(request, 'tax-regulatory-people-services.html' )
+
+# class_based views of the insight
+class postslist(generic.ListView):
+    queryset = posts.objects.filter(status=1).order_by('-created_on')
+    template_name = 'insight.html'
+    paginate_by = 10
+
+class postdetail(generic.DetailView):
+    model = posts
+    template_name = "insight_post.html"
